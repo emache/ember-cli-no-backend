75a1e9dce965cb06ec82dfdefba65476f29ac6b6
Marie Lu Vinh <mluvinh@gmail.com>
2015-04-18 11:56:45 +0100

    content security policy fix via add-on

--- config/environment.js
+++ config/environment.js
@@ -1,23 +1,34 @@
 /* jshint node: true */
 
 module.exports = function(environment) {
-  var ENV = {
-    modulePrefix: 'dummy-website',
-    environment: environment,
-    baseURL: '/',
-    locationType: 'auto',
-    EmberENV: {
-      FEATURES: {
-        // Here you can enable experimental features on an ember canary build
-        // e.g. 'with-controller': true
-      }
-    },
+    var ENV = {
+      modulePrefix: 'dummy-website',
+      environment: environment,
+      baseURL: '/',
+      locationType: 'auto',
+      EmberENV: {
+        FEATURES: {
+          // Here you can enable experimental features on an ember canary build
+          // e.g. 'with-controller': true
+        }
+      },
 
-    APP: {
-      // Here you can pass flags/options to your application instance
-      // when it is created
-    }
-  };
+      APP: {
+        // Here you can pass flags/options to your application instance
+        // when it is created
+      }
+    };
+
+    ENV.contentSecurityPolicy = {
+        'default-src': "'none'",
+        'script-src': "'self' https://cdn.mxpnl.com", // Allow scripts from https://cdn.mxpnl.com
+        'font-src': "'self' http://fonts.gstatic.com", // Allow fonts to be loaded from http://fonts.gstatic.com
+        'connect-src': "'self' https://api.mixpanel.com http://custom-api.local", // Allow data (ajax/websocket) from api.mixpanel.com and custom-api.local
+        'img-src': "'self'",
+        'style-src': "'self' 'unsafe-inline' http://fonts.googleapis.com", // Allow inline styles and loaded CSS from http://fonts.googleapis.com 
+        'media-src': "'self'"
+    };
+
 
   if (environment === 'development') {
     // ENV.APP.LOG_RESOLVER = true;
@@ -44,4 +55,5 @@ module.exports = function(environment) {
   }
 
   return ENV;
+
 };
