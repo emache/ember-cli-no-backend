a196b2e7a43e67f30eb9c0df16312695d91aa72c
Marie Lu Vinh <mluvinh@gmail.com>
2015-05-04 18:31:40 +0100

    load .md files and generate index.json through broccoli

--- Brocfile.js
+++ Brocfile.js
@@ -1,8 +1,62 @@
 /* global require, module */
 
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
+var funnel = require('broccoli-funnel');
+var marked = require('marked');
+var yaml = require('js-yaml');
+var fs = require('fs');
+var glob = require('glob');
 
 var app = new EmberApp();
+
+var loadFile = function (ymlName) {
+    var data = fs.readFileSync(ymlName, "utf8");
+    var project = {};
+    var dividerPos = data.indexOf("\n---\n");
+    project.description = marked(data.substr(dividerPos + 6));
+
+    var doc = yaml.safeLoad(data.substr(0, dividerPos));
+    project.colour = doc.colour;
+    project.date = doc.date;
+    project.title = doc.title;
+    project.slug = doc.slug;
+    project.previewImgUrl = doc.previewImgUrl;
+    project.featuredImgUrl = doc.featuredImgUrl;
+    
+    return project;
+};
+
+var convertProjectsFiles = function() {
+    var index = [];
+    var ymls = glob('data/projects/*.md', {sync: true});
+    
+    console.log("Converting " + ymls.length + " files");
+
+    ymls.forEach(function (ymlName) {
+        // Load
+        var project = loadFile(ymlName);
+        var fileName = ymlName.replace('data/projects/', '').replace('.md', '');
+
+        // Store summary for index
+        index.push(
+            {
+                title: project.title,
+                colour: project.colour,
+                date: project.date,
+                previewImgUrl: project.previewImgUrl,
+                slug: fileName
+            });
+    });
+
+    fs.writeFileSync('api/projects/index.json', JSON.stringify(index));
+};
+
+convertProjectsFiles();
+
+var projectsData = funnel('api/projects', {
+    destDir: 'api/projects/'
+});
+
 
 // Use `app.import` to add additional libraries to the generated
 // output files.
@@ -17,4 +71,4 @@ var app = new EmberApp();
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
 
-module.exports = app.toTree();
+module.exports = app.toTree([projectsData]);
